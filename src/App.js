08b4cffbb7245502b1e07410ae08c9f1a1f0cf60
amisha-lathiya <amisha-lathiya@gmail.com>
2024-04-06 18:37:26 +0530

    create pages and rotes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Singin from './pages/Singin'
+import Dashboard from './pages/Dashboard'
+import Singup from './Singup'
+
+
 
 const App = () => {
   return (
-    <div className='text-red-900'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sing-in" element={<Singin />}></Route>
+        <Route path="/sing-up" element={<Singup />}></Route>
+        <Route path="/DashBoard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
